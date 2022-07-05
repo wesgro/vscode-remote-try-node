29dbd6412c186a5c7ed657fd8bb39330a71303ef
Jacob Ketcheson <snowcave@gmail.com>
2022-07-05 04:57:58 +0000

    made breakpoint HOC

---
+++ dist/use-media-query.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+/**
+ * Custom hook that tells you whether a given media query is active.
+ *
+ * Inspired by https://usehooks.com/useMedia/
+ * https://gist.github.com/gragland/ed8cac563f5df71d78f4a1fefa8c5633
+ */
+export function useMediaQuery(query) {
+    const [matches, setMatches] = useState(false);
+    useEffect(() => {
+        const mediaQuery = window.matchMedia(query);
+        setMatches(mediaQuery.matches);
+        const handler = (event) => setMatches(event.matches);
+        mediaQuery.addEventListener("change", handler);
+        return () => mediaQuery.removeEventListener("change", handler);
+    }, [] // Empty array ensures effect is only run on mount and unmount
+    );
+    return matches;
+}
+/**
+ * Get a set of boolean representing which breakpoint is active
+ * and which breakpoints are inactive.
+ *
+ * Inspired by: https://github.com/contra/react-responsive/issues/162#issuecomment-592082035
+ */
+export function useBreakpoints() {
+    const breakpoints = {
+        isXs: useMediaQuery("(max-width: 640px)"),
+        isSm: useMediaQuery("(min-width: 641px) and (max-width: 768px)"),
+        isMd: useMediaQuery("(min-width: 769px) and (max-width: 1024px)"),
+        isLg: useMediaQuery("(min-width: 1025px)"),
+        active: "xs",
+    };
+    if (breakpoints.isXs)
+        breakpoints.active = "xs";
+    if (breakpoints.isSm)
+        breakpoints.active = "sm";
+    if (breakpoints.isMd)
+        breakpoints.active = "md";
+    if (breakpoints.isLg)
+        breakpoints.active = "lg";
+    return breakpoints;
+}
---
+++ src/stories/use-media-query.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+/**
+ * Custom hook that tells you whether a given media query is active.
+ *
+ * Inspired by https://usehooks.com/useMedia/
+ * https://gist.github.com/gragland/ed8cac563f5df71d78f4a1fefa8c5633
+ */
+export function useMediaQuery(query: string) {
+  const [matches, setMatches] = useState<boolean>(false);
+  useEffect(
+    () => {
+      const mediaQuery = window.matchMedia(query);
+      setMatches(mediaQuery.matches);
+      const handler = (event: MediaQueryListEvent) => setMatches(event.matches);
+      mediaQuery.addEventListener("change", handler);
+      return () => mediaQuery.removeEventListener("change", handler);
+    },
+    [] // Empty array ensures effect is only run on mount and unmount
+  );
+  return matches;
+}
+
+interface BreakpointValues {
+  isXs: boolean;
+  isSm: boolean;
+  isMd: boolean;
+  isLg: boolean;
+  active: "xs" | "sm" | "md" | "lg";
+}
+/**
+ * Get a set of boolean representing which breakpoint is active
+ * and which breakpoints are inactive.
+ *
+ * Inspired by: https://github.com/contra/react-responsive/issues/162#issuecomment-592082035
+ */
+export function useBreakpoints() {
+  const breakpoints: BreakpointValues = {
+    isXs: useMediaQuery("(max-width: 640px)"),
+    isSm: useMediaQuery("(min-width: 641px) and (max-width: 768px)"),
+    isMd: useMediaQuery("(min-width: 769px) and (max-width: 1024px)"),
+    isLg: useMediaQuery("(min-width: 1025px)"),
+    active: "xs",
+  };
+  if (breakpoints.isXs) breakpoints.active = "xs";
+  if (breakpoints.isSm) breakpoints.active = "sm";
+  if (breakpoints.isMd) breakpoints.active = "md";
+  if (breakpoints.isLg) breakpoints.active = "lg";
+  return breakpoints;
+}
